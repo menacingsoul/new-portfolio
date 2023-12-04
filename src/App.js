810cc5a553a6ac4c88596b9d6ef917150d698e61
menacingsoul <adarsh14103@gmail.com>
2023-12-05 02:58:32 +0530

    resume added

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import { Banner } from './components/Banner';
 import { Skills } from './components/Skills';
 import { Project } from './components/Project';
 import { Contact } from './components/Contact';
-
 import { Footer } from './components/Footer';
 function App() {
   return (
